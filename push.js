866a4d6675a09576c1ae88e2bdbbf31e6c1db7c4
Daniel Bengtsson <daniel@bengtssons.info>
2016-03-23 22:17:49 +0100

    Added check before push

--- push.js
+++ push.js
@@ -16,31 +16,70 @@ function push() {
 	}
 
 	// Ready to try to push
-	
+	// Assume remote is already setup in can_push()
 	var wc = review.branch.getWorkCopy();
-	var remotes = wc.run("remote");
-	if( remotes.split("\n").indexOf("target") != -1 ) {
-	    wc.run("remote", "remove", "target");
-	}
-	var remote_base = critic.storage.get('remote_base');
-	// FIXME: Not a guarantee that the repo name is also used at base
-	wc.run("remote", "add", "target", remote_base + "/" + review.repository.name);
-
 	var out = wc.run("push", "target", "--porcelain", "HEAD:refs/heads/master");
 	if( out.split('\n')[1][0] == '=' ) {
 	    throw "Already integrated!";
 	}
 	review.close();
-    
+	
 	writeln(JSON.stringify({ status: "ok" }));
     } catch (error) {
 	if( error instanceof critic.CriticError ) {
 	    throw error;
 	}
-	    
+	
 	writeln(JSON.stringify({ status: "failure",
 				 code: "willnotpush",
 				 title: "Will not push to master",
 				 message: String(error) }));
     }
+}
+
+function add_remote(review) {
+    var wc = review.branch.getWorkCopy();
+    var remotes = wc.run("remote");
+    if( remotes.split("\n").indexOf("target") != -1 ) {
+	wc.run("remote", "remove", "target");
+    }
+    var remote_base = critic.storage.get('remote_base');
+    // FIXME: Not a guarantee that the repo name is also used at base
+    wc.run("remote", "add", "target", remote_base + "/" + review.repository.name);
+    return [wc, remote_base]
+}
+
+function can_push() {
+    var data = JSON.parse(read());
+    
+    writeln("200");
+    writeln("Content-Type: text/json");
+    writeln("");
+    
+    try {
+	var review = new critic.Review(data.review_id);
+	
+	// Should already have been checked - but verify
+	if( ! review.progress.accepted ) {
+	    throw "Review is not accepted";
+	}
+	
+	// Add target and check if the review branch is based on it
+	var res = add_remote(review);
+	var wc = res[0];
+	var remote_base = res[1];
+	wc.run("fetch", "target", "master");
+	var merge_base = wc.run("merge-base", "HEAD", "target/master");
+	var target_master = wc.run("rev-parse", "target/master");
+	writeln(JSON.stringify({ status: "ok", integratable: merge_base == target_master }));
+    } catch (error) {
+	if( error instanceof critic.CriticError ) {
+	    throw error;
+	}
+	
+	writeln(JSON.stringify({ status: "failure",
+				 code: "willnotpush",
+				 title: "Will not push to master",
+				 message: String(error) }));
+    }	
 }
--- resources/integrate.js
+++ resources/integrate.js
@@ -11,22 +11,60 @@ $( document ).ready(function() {
 function add_button(result) {
     if( result.accepted ) {
 	critic.buttons.add({title: "Integrate",
-			    onclick: integrate,
+			    onclick: can_push,
 			    scope: "global"})
     }
 }
 
-function integrate() {
+function can_push() {
     var operation = new critic.Operation({
-      action: "Push to master",
-      url: "Integrate/push",
-      data: { review_id: critic.review.id },
-      wait: "Pushing...",
-      callback: done
+	action: "Check if integration is possible",
+	url: "Integrate/can_push",
+	data: { review_id: critic.review.id },
+	wait: "Checking...",
+	callback: do_push_dialog
     });
     operation.execute();
 }
 
+function do_push_dialog(res) {
+    console.log(res);
+    if(res == null) {
+	return;
+    }
+    if(!res.integratable) {
+	var dialog = $('<div title="Needs rebase">' +
+		       '<p>The current review branch is not based on the current master. Please rebase it before trying to integrate.</p></div>');
+	dialog.dialog({
+	    width: 500,
+	    buttons: { ok: function() { $(this).dialog("close"); }}
+	});
+	return;
+    }
+   
+    var dialog = $('<div title="Push changes to master">' +
+		   '<p>Review branch checked and seem to be OK. Ready to push?</p></div>');
+    dialog.dialog({
+	width: 500,
+	buttons: {
+	    "Push": integrate,
+	    "Abort": function() { dialog.dialog("close"); }
+	}});
+
+    function integrate() {
+	dialog.dialog("close");
+	var operation = new critic.Operation({
+	    action: "Push to master",
+	    url: "Integrate/push",
+	    data: { review_id: critic.review.id },
+	    wait: "Pushing...",
+	    callback: done
+	});
+	operation.execute();
+    }
+}
+
 function done(res) {
-    location.reload();
+    if( res != null )
+	location.reload();
 }
